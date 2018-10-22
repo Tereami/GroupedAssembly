bc04c1fccf72b358d03137dcb9841d8c943e8a03
Fedor Zhekov <zhekovfi@gmail.com>
2018-10-22 21:15:49 +0300

    отрефакторил проверку на врзможность включения в сборку

---
+++ GroupedAssembly/ElementExt.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+
+namespace GroupedAssembly
+{
+    public static class ElementExt
+    {
+        public static bool CanAssembling(this Element e)
+        {
+            return AssemblyInstance.AreElementsValidForAssembly(e.Document, new List<ElementId> {e.Id},
+                ElementId.InvalidElementId);
+        }
+    }
+}
