f05ffd05ad268a38554f7cecac5423aad3db4172
Александр ЗУев <undress.my.future@gmail.com>
2019-12-05 13:20:00 +0300

    Добавлен текст лицензии

--- GroupedAssembly/CommandSuperAssembly.cs
+++ GroupedAssembly/CommandSuperAssembly.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿//Данный код опубликован под лицензией Creative Commons Attribution-NonCommercial-ShareAlike.
+//Разрешено редактировать, изменять и брать данный код за основу для производных в некоммерческих целях,
+//при условии указания авторства и если производные лицензируются на тех же условиях.
+//Программа поставляется "как есть". Автор не несет ответственности за возможные последствия её использования.
+//Зуев Александр, 2019, все права защищены.
+//This code is listed under the Creative Commons Attribution-NonCommercial-ShareAlike license.
+//You may redistribute, remix, tweak, and build upon this work non-commercially, 
+//as long as you credit the author by linking back and license your new creations under the same terms.
+//This code is provided 'as is'. Author disclaims any implied warranty. 
+//Zuev Aleksandr, 2019, all rigths reserved.
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Autodesk.Revit.DB;
